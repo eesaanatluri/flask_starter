66f9783081bae39165e1be619d5cc6b1633de178
Eesaan Atluri <atlurie@uab.edu>
2018-05-22 18:28:15 -0400

    Delete prev test route and added login-manager.

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_login import LoginManager
+from flask_migrate import Migrate
 # local imports
 from config import app_config
 
 # db variable initialization
 db = SQLAlchemy()
+
+# creating login manager object
+login_manager = LoginManager()
 
 # Load the Config File
 def create_app(config_name):
@@ -15,8 +19,8 @@ def create_app(config_name):
     app.config.from_pyfile('config.py')
     db.init_app(app)
 
-    @app.route('/')
-    def hello_world():
-        return 'Hello, World!'
+    login_manager.init_app(app)
+    login_manager.loginmessage = "you must be logged in to access this page."
+    login_manager.login_view = "auth.login"
 
     return app

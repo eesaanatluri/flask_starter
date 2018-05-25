8eb6fff64e1bedf438241c487cf805a251b3d0d8
Eesaan Atluri <atlurie@uab.edu>
2018-05-25 16:01:06 -0400

    added error handlers and turned off sqlalchemy track modifications

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_bootstrap import Bootstrap
+from flask import Flask, render_template, abort
+
 # local imports
 from config import app_config
 
@@ -16,6 +18,7 @@ login_manager = LoginManager()
 def create_app(config_name):
     # initialize the app
     app = Flask(__name__, instance_relative_config=True)
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
 
@@ -39,5 +42,21 @@ def create_app(config_name):
 
     from .home import home as home_blueprint
     app.register_blueprint(home_blueprint)
+
+    @app.errorhandler(403)
+    def forbidden(error):
+        return render_template('errors/403.html', title='Forbidden'), 403
+
+    @app.errorhandler(404)
+    def page_not_found(error):
+        return render_template('errors/404.html', title='Page Not Found'), 404
+
+    @app.errorhandler(500)
+    def internal_server_error(error):
+        return render_template('errors/500.html', title='Server Error'), 500
+
+    @app.route('/500')
+    def error():
+        abort(500)
 
     return app
